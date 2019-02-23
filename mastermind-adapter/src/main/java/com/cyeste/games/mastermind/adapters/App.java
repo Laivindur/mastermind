d3347afe092c56675652c71d3592f96334119c4e
Christian Yeste <christianyv@gmail.com>
2019-02-23 13:43:01 +0100

    Clean architecture candidate as maven modules

---
+++ mastermind-adapter/src/main/java/com/cyeste/games/mastermind/adapters/App.java
@@ -0,0 +1,13 @@
+package com.cyeste.games.mastermind.adapters;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
