ae216b2c0fc7ee06c3a3fdaeebd74ebfc6832819
Christian Yeste <christianyv@gmail.com>
2019-02-23 16:13:17 +0100

    Added some comments to Peg class

--- mastermind-domain/src/main/java/com/cyeste/games/mastermind/domain/Peg.java
+++ mastermind-domain/src/main/java/com/cyeste/games/mastermind/domain/Peg.java
@@ -13,8 +13,25 @@ public class Peg {
 		this.color = color;		
 	}
 	
-	public boolean equals(Peg peg) {
-		return peg != null && color.equals(peg.color);
+	public boolean equals(Object peg) {
+		//The comparable is not null
+		return peg != null && 
+				//is instance of the same class
+				Peg.class.isInstance(peg) &&
+				//both are the same instance or both has the same color
+				(this == peg || color.equals(Peg.class.cast(peg).color));
+	}
+	
+
+	/*
+	 *For us to use Hashed data structures such as LinkedHashSet<> or LinkedHashMap<> 
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((color == null) ? 0 : color.hashCode());
+		return result;
 	}
 
 	public static enum Color {
