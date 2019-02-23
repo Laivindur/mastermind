b30655124f47c0cdf59830692acbfb2a09f83d1d
Christian Yeste <christianyv@gmail.com>
2019-02-23 16:54:42 +0100

    Added more Pattern unit test

--- mastermind-domain/src/test/java/com/cyeste/games/mastermind/domain/PatternCreationTest.java
+++ mastermind-domain/src/test/java/com/cyeste/games/mastermind/domain/PatternCreationTest.java
@@ -1,7 +1,14 @@
 package com.cyeste.games.mastermind.domain;
 
-import java.util.Collections;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.BlockJUnit4ClassRunner;
@@ -9,6 +16,16 @@ import org.junit.runners.BlockJUnit4ClassRunner;
 @RunWith(BlockJUnit4ClassRunner.class)
 public class PatternCreationTest {
 
+	private static final Set<Peg> DEFAULT_PEGS = new LinkedHashSet<Peg>();
+	
+	@BeforeClass
+	public static void setUp() {
+		DEFAULT_PEGS.add(new Peg(Peg.Color.BLUE));
+		DEFAULT_PEGS.add(new Peg(Peg.Color.GREEN));
+		DEFAULT_PEGS.add(new Peg(Peg.Color.PINK));
+		DEFAULT_PEGS.add(new Peg(Peg.Color.YELLOW));
+	}
+	
 	@Test(expected=IllegalArgumentException.class)
 	public void createPatternWithoutPegs() {
 		new Pattern(null);
@@ -18,4 +35,17 @@ public class PatternCreationTest {
 	public void createPatternWithEmptyPegs() {
 		new Pattern(Collections.emptySet());
 	}
+	
+	@Test
+	public void createdSuccessfully(){
+		Pattern pattern = new Pattern(DEFAULT_PEGS);
+		assertNotNull(pattern);
+		assertEquals(DEFAULT_PEGS.size(), pattern.length());
+	}
+	
+	@Test
+	public void clonePattern() {
+		Pattern pattern = new Pattern(DEFAULT_PEGS);
+		Pattern clone = pattern.clone();
+		assertFalse(pattern == clone);	}
 }
