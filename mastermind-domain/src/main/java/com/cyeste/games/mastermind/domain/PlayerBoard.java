27dbb37e9787a4d22cbcca5c317d4d746a557418
Christian Yeste <christianyv@gmail.com>
2019-02-23 23:54:15 +0100

    Added Id to PlayerBoard but no builder

--- mastermind-domain/src/main/java/com/cyeste/games/mastermind/domain/PlayerBoard.java
+++ mastermind-domain/src/main/java/com/cyeste/games/mastermind/domain/PlayerBoard.java
@@ -1,20 +1,29 @@
 package com.cyeste.games.mastermind.domain;
 
+import java.io.Serializable;
+
 import com.cyeste.games.mastermind.domain.utils.Validations;
 
 public class PlayerBoard {
 
-	private static final String TO_STRING = "{player: %s, board: %s, coder: %s}";
+	private static final String TO_STRING = "{id: \"%s\", player: %s, board: %s, coder: %s}";
+	private final Serializable id;
 	private final Player player;
 	private final DecodingBoard board;
 	private final boolean coder;
 	
-	PlayerBoard(Player player, DecodingBoard board, boolean coder) {
+	PlayerBoard(Serializable id, Player player, DecodingBoard board, boolean coder) {
 		Validations.whenNull(player).throwIllegalArgumentException("Player in PlayerBoards is required");
 		Validations.whenNull(board).throwIllegalArgumentException("Board in PlayerBoards is required");
+		Validations.whenNull(id).throwIllegalArgumentException("PlayerBoard idz is required");
+
+		this.id = id;
 		this.player = player;
 		this.board = board;
 		this.coder = coder;
+	}
+	public Serializable getId() {
+		return id;
 	}
 	
 	public DecodingBoard getBoard(){
@@ -29,17 +38,17 @@ public class PlayerBoard {
 		return player;
 	}
 	
-	public static PlayerBoard playerCodeMaker(Player player, DecodingBoard board) {
-		return new PlayerBoard(player, board, Boolean.TRUE);
+	public static PlayerBoard playerCodeMaker(Serializable id, Player player, DecodingBoard board) {
+		return new PlayerBoard(id, player, board, Boolean.TRUE);
 	}
 	
-	public static PlayerBoard playerCodeBreaker(Player player, DecodingBoard board) {
-		return new PlayerBoard(player, board, Boolean.FALSE);
+	public static PlayerBoard playerCodeBreaker(Serializable id, Player player, DecodingBoard board) {
+		return new PlayerBoard(id,player, board, Boolean.FALSE);
 	}
 	
 	@Override
 	public String toString() {
-		return String.format(TO_STRING, player,board,String.valueOf(coder));
+		return String.format(TO_STRING, id, player,board,String.valueOf(coder));
 	}
 	
 }
--- mastermind-domain/src/test/java/com/cyeste/games/mastermind/domain/PlayerBoardCreationTest.java
+++ mastermind-domain/src/test/java/com/cyeste/games/mastermind/domain/PlayerBoardCreationTest.java
@@ -15,24 +15,29 @@ public class PlayerBoardCreationTest {
 
 	private static final String DEFAULT_NAME = "Han Solo";
 	private final static Logger LOGGER = Logger.getLogger(BoardGameCreationTest.class.getName());
-	private static final Player DEFAULT_PLAYER = Player.builder().name(DEFAULT_NAME).build();
+	private static final Player DEFAULT_PLAYER = Player.builder().id("id").name(DEFAULT_NAME).build();
 	
 	private static final DecodingBoard DEFAULT_BOARD = generateBoard(Peg.Color.GREEN, Peg.Color.GREEN, Peg.Color.GREEN, Peg.Color.YELLOW);
 
 
 	@Test(expected=IllegalArgumentException.class)
 	public void codeMakerWithNoPlayer() {
-		PlayerBoard.playerCodeMaker(null, null);
+		PlayerBoard.playerCodeMaker(null,null, null);
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
 	public void codeMakerWithNoBoard() {
-		PlayerBoard.playerCodeMaker(DEFAULT_PLAYER, null);
+		PlayerBoard.playerCodeMaker(null,DEFAULT_PLAYER, null);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void codeMakerWithNoId() {
+		PlayerBoard.playerCodeMaker(null,DEFAULT_PLAYER, DEFAULT_BOARD);
 	}
 	
 	@Test
 	public void codeMaker() {
-		PlayerBoard playerBoard = PlayerBoard.playerCodeMaker(DEFAULT_PLAYER, DEFAULT_BOARD);
+		PlayerBoard playerBoard = PlayerBoard.playerCodeMaker("id",DEFAULT_PLAYER, DEFAULT_BOARD);
 		Assert.assertNotNull(playerBoard);
 		Assert.assertTrue(playerBoard.isCoder());
 		LOGGER.info("PlayingCodeMaker: " + playerBoard);
@@ -40,17 +45,22 @@ public class PlayerBoardCreationTest {
 	
 	@Test(expected=IllegalArgumentException.class)
 	public void codeBreakerWithNoPlayer() {
-		PlayerBoard.playerCodeBreaker(null, null);
+		PlayerBoard.playerCodeBreaker(null,null, null);
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
 	public void codeBreakerWithNoBoard() {
-		PlayerBoard.playerCodeBreaker(DEFAULT_PLAYER, null);
+		PlayerBoard.playerCodeBreaker(null,DEFAULT_PLAYER, null);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void codeBreakerWithNoId() {
+		PlayerBoard.playerCodeBreaker(null,DEFAULT_PLAYER, DEFAULT_BOARD);
 	}
 	
 	@Test
 	public void codeBreaker() {
-		PlayerBoard playerBoard = PlayerBoard.playerCodeBreaker(DEFAULT_PLAYER, DEFAULT_BOARD);
+		PlayerBoard playerBoard = PlayerBoard.playerCodeBreaker("id",DEFAULT_PLAYER, DEFAULT_BOARD);
 		Assert.assertNotNull(playerBoard);
 		Assert.assertFalse(playerBoard.isCoder());
 		LOGGER.info("PlayerCodeBreaker: " + playerBoard);
